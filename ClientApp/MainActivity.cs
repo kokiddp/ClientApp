c1474aa4fc58c5b97d07ed3635b28ee29a60961b
kokiddp <kokiddp@gmail.com>
2016-12-04 16:42:19 +0100

    Aggiungere i file di progetto.

---
+++ ClientApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ClientApp
+{
+    [Activity(Label = "ClientApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
